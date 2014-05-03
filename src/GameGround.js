83971a44f4ad2b12fd10104d6e19d586a2acc8b0
takorstark <takorstark@kuwin-134-213.kuwin.ku.ac.th>
2014-05-03 21:47:15 +0700

    make movement for the game ground.

--- src/GameGround.js
+++ src/GameGround.js
@@ -1,9 +1,46 @@
 var GameGround = cc.Sprite.extend({
 	ctor: function(){
 		this._super();
-		this.initWithFile( ' images/game_ground.png ' );
 
-	}
+        this.ground1 = cc.Sprite.create( ' images/game_ground.png ' );
+        this.ground1.setAnchorPoint( new cc.Point( 0, 1 ) );
+        this.ground1.setPosition( new cc.Point( -400, 80 ) );
+        this.addChild( this.ground1 );
+
+        this.ground2 = cc.Sprite.create( ' images/game_ground.png ' );
+        this.ground2.setAnchorPoint( new cc.Point( 0, 1 ) );
+        this.ground2.setPosition( new cc.Point( 400, 80 ) );
+        this.addChild( this.ground2 );
+
+        this.ground3 = cc.Sprite.create( ' images/game_ground.png ' );
+        this.ground3.setAnchorPoint( new cc.Point( 0, 1 ) );
+        this.ground3.setPosition( new cc.Point( 1200, 80 ) );
+        this.addChild( this.ground3 );
+
+
+	},
+
+	update: function( dt ) {
+		this.pos1 = this.ground1.getPositionX();
+		this.pos2 = this.ground2.getPositionX();
+		this.pos3 = this.ground3.getPositionX();
+
+		this.ground1.setPositionX( this.pos1 - 5 );
+		this.ground2.setPositionX( this.pos2 - 5 );
+		this.ground3.setPositionX( this.pos3 - 5 );
+
+		if( this.pos1 < -1600 ){
+			this.ground1.setPositionX( 790 );
+		}
+		if( this.pos2 < -1600 ){
+			this.ground2.setPositionX( 790 );
+		}
+		if( this.pos3 < -1600 ){
+			this.ground3.setPositionX( 790 );
+		}
+		
+	},
+
 
 })
 	
--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,11 +1,12 @@
 var GameLayer = cc.LayerColor.extend({
     init: function() {
+
         this._super( new cc.Color4B( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
 
         this.player = new DumpIronman();
         this.player.setPosition( new cc.Point( screenWidth / 4, screenHeight / 3 ) );
-        this.addChild( this.player, 1 );
+        this.addChild( this.player, 1 )
         this.player.scheduleUpdate();
 
         this.ground = new GameGround();
@@ -13,32 +14,47 @@ var GameLayer = cc.LayerColor.extend({
         this.addChild( this.ground );
         this.ground.scheduleUpdate();
 
+
         this.setKeyboardEnabled( true );
 
         this.state = GameLayer.STATES.FRONT;
 
-        this.rock = null;
+        // this.rock = null;
+
+        this.j = 0;
 
         this.scheduleUpdate();
 
-        return true
+        return true;
     },
 
     onKeyDown: function( e ){
         this.player.start();
-        
         if( this.state == GameLayer.STATES.FRONT ) {
             this.startGame();
             this.state = GameLayer.STATES.STARTED;
         } else if( this.state == GameLayer.STATES.STARTED ) {
-            if( e == cc.KEY.space )    
-                this.player.jump();
+            if( e == cc.KEY.space && !this.pressed ){
+                this.pressed = true;
+                 this.player.jump();
+                 this.j++;
+            }
+               
+        } 
+    },
+
+    onKeyUp : function ( e ) {
+        if( this.state == GameLayer.STATES.STARTED ) {
+            if( e == cc.KEY.space&& this.pressed){
+                this.pressed = false;
+            }    
+               
         } 
     },
 
     createRock: function() {
         this.rock = new Rock();
-        this.rock.setPosition( new cc.Point( 900, 310 ) );
+        this.rock.setPosition( new cc.Point( 900, 315 ) );
         this.addChild( this.rock );
         this.rock.scheduleUpdate();
     },
